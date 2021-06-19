03dac655bc3286cc9fef7db66fdbd736776bdc6a
yongjang cho <dydwkd48670@gmail.com>
2021-06-19 23:33:37 +0900

    Create baekjoon11279.py

---
+++ baekjoon/baekjoon11279.py
@@ -0,0 +1,15 @@
+import sys
+import heapq
+
+n= int(input())
+heap=[]
+for i in range(n):
+    temp=int(sys.stdin.readline())
+    if temp==0:
+        if len(heap)==0:
+            print(0)
+        else:
+            c=heapq.heappop(heap)[1]
+            print(c)
+    else:
+        heapq.heappush(heap,(-temp,temp))
