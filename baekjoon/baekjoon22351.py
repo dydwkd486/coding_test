6b6c0f25b65a84d4ae9784b715aba3466a94e97a
yongjang cho <dydwkd48670@gmail.com>
2021-11-06 19:32:36 +0900

    Create baekjoon22351.py

---
+++ baekjoon/baekjoon22351.py
@@ -0,0 +1,16 @@
+def number(N):
+    length = len(N)
+    A_lst=[int(N[0]),int(N[:2]),int(N[:3])]
+    for A in A_lst:
+        B= A
+        new_num =''
+        while len(new_num)<length:
+            new_num +=str(B)
+            # print(new_num)
+            if new_num==N:
+                return A,B
+            B+=1
+    return N,N
+
+N=input()
+print(*number(N))
