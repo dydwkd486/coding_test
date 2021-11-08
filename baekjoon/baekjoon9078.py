10d950c599099994019f52fe4a5232b8e57c7bad
yongjang cho <dydwkd48670@gmail.com>
2021-11-08 19:26:18 +0900

    Create baekjoon9078.py

---
+++ baekjoon/baekjoon9078.py
@@ -0,0 +1,18 @@
+t= int(input())
+
+
+for _ in range(t):
+    _ = input()
+    n= list(map(int,input().split(" ")))
+    count=0
+    for i in range(len(n)):
+
+        for j in range(i,len(n),1):
+            if n[i]>n[j]:
+                count+=1
+    if count%2==0:
+        print("YES")
+    else:
+        print("NO")
+    
+
