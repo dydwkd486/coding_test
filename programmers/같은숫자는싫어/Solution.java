124b73cc0dec53a8fd818f6e4904cd825439e2ff
yongjang cho <dydwkd48670@gmail.com>
2022-08-30 23:56:36 +0900

    Create Solution.java

---
+++ programmers/같은숫자는싫어/Solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<answer.length;j++){
+              if(answer[j]==arr[i]){
+                  
+              }  
+            }
+            }
+        }
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
