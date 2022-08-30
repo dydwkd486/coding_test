0e322b9fc10b036e134e2716df9f7dd7dc0de84d
yongjang cho <dydwkd48670@gmail.com>
2022-08-31 00:26:54 +0900

    Update Solution.java

--- programmers/같은숫자는싫어/Solution.java
+++ programmers/같은숫자는싫어/Solution.java
@@ -2,18 +2,15 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        int[] answer = {};
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<answer.length;j++){
-              if(answer[j]==arr[i]){
-                  
-              }  
-            }
-            }
+        int[] answer = new int[arr.length];
+        answer[0] = arr[0];
+        int count =0;
+        for(int i = 1; i<arr.length;i++){
+            if(answer[count]==arr[i]) continue;
+            answer[++count] =arr[i];
         }
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
+        answer = Arrays.copyOfRange(answer,0,count+1);
         return answer;
     }
 }
