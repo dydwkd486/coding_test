2bcc22d31e0734e1310bc4e79b8a9a51fb15f66c
yongjang cho <dydwkd48670@gmail.com>
2022-09-10 18:57:28 +0900

    Create baekjoon16234_1.py

---
+++ baekjoon/python/baekjoon16234_1.py
@@ -0,0 +1,60 @@
+import sys
+from collections import deque
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+input = sys.stdin.readline
+
+n,l,r = list(map(int,input().split()))
+graph = [list(map(int,input().split())) for _ in range(n)]
+result =0
+def bfs(visited,queue,visited_list):
+    while queue:
+        y,x = queue.popleft()
+        for i in range(4):
+            vy = y+dy[i]
+            vx = x+dx[i]
+            if -1<vy<n and -1<vx<n and visited[vy][vx]==False:
+                if l<=abs(graph[vy][vx]-graph[y][x])<=r:
+                    visited[vy][vx]=True
+                    visited_list.append([vy,vx])
+                    queue.append([vy,vx])
+    # print(sum)
+    
+    # 가능한 경우를 다 찾음
+    # 이제 변경해야함.
+    if len(visited_list) > 1:
+        changed_value = sum([graph[y][x] for y, x in visited_list])//len(visited_list)
+        for y,x in visited_list:
+                    graph[y][x] = changed_value
+        return True
+    else:
+        return False
+
+# 입력 완료
+
+# dfs 구현하기
+# print(graph)
+
+# 2000일을 돌수 있게 해야함.
+# 초기화 
+for _ in range(2000):
+    visited = [[False]*n for _ in range(n)]
+    total_bool = False
+    for i in range(n):
+        for j in range(n):
+            if visited[i][j] == False:
+                visited[i][j]=True
+                queue = deque()
+                queue.append([i,j])
+                visited_list = [[i,j]]
+                change_bool = bfs(visited,queue,visited_list)
+                if change_bool:
+                    total_bool =True
+    if total_bool:
+        result +=1
+    else:
+        break
+            
+print(result)
+            
+
