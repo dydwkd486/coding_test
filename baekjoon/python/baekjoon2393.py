dd262b4c022cfb4c25b634e241585a09e5d8028d
yongjang cho <dydwkd48670@gmail.com>
2023-01-05 19:47:11 +0900

    20230105 update

---
+++ baekjoon/python/baekjoon2393.py
@@ -0,0 +1,36 @@
+print("  ___  ___  ___")
+print("  | |__| |__| |")
+print("  |           |")
+print("   \_________/")
+print("    \_______/")
+print("     |     |")
+print("     |     |")
+print("     |     |")
+print("     |     |")
+print("     |_____|")
+print("  __/       \__")
+print(" /             \\")
+print("/_______________\\")
+
+'''
+
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. IDSAMPLE.
+       ENVIRONMENT DIVISION.
+       PROCEDURE DIVISION.
+       DISPLAY '  ___  ___  ___'
+       DISPLAY '  | |__| |__| |'
+       DISPLAY '  |           |'
+       DISPLAY '   \_________/'
+       DISPLAY '    \_______/'
+       DISPLAY '     |     |'
+       DISPLAY '     |     |'
+       DISPLAY '     |     |'
+       DISPLAY '     |     |'
+       DISPLAY '     |_____|'
+       DISPLAY '  __/       \__'
+       DISPLAY ' /             \'
+       DISPLAY '/_______________\'
+       STOP RUN.
+
+'''
